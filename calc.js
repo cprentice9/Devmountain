830074c2f16417530378c36bb623e8328f7e50c1
Connor Prentice <connurp@gmail.com>
2022-02-19 15:12:10 -0600

    final calculator version

--- calc.js
+++ calc.js
@@ -14,8 +14,22 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	if(mathSymbol == "+") {
+		console.log(num1 + num2);
+	}
 
-	//one more time for the folks in the back This line closes the connection to the command line interface.
+	if(mathSymbol == "-") {
+		console.log(num1 - num2);
+	}
+
+	if(mathSymbol == "*") {
+		console.log(num1 * num2);
+	}
+	
+	if(mathSymbol == "/") {
+		console.log(num1 / num2);
+	}
+	//This line closes the connection to the command line interface.
 	reader.close()
 
 });
