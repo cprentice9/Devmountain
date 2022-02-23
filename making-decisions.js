01e61ae5960d57f767ce63dff96faade58d31647
Connor Prentice <connurp@gmail.com>
2022-02-22 22:44:04 -0600

    first draft on this trying it out.

---
+++ making-decisions.js
@@ -0,0 +1,13 @@
+let jonSnowAttack = 25
+let jamieLannisterAttack =35
+
+let jonSnowHealth = 100
+let jonSnowDefense = 10
+
+if(jonSnowAttack > jamieLannisterAttack){
+    console.log('Jon Snow has a greater attack power than Jamie Lannister')
+} else if(jamieLannisterAttack > jonSnowAttack) {
+    console.log("Jamie Lannister has a greater attack power than Jon Snow")
+} else {
+    console.log('Jon and Jamie are the samie')
+}
