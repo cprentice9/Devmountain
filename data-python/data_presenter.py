b8513024c1b67cd6d38375107cb91d0331f25614
Connor Prentice <connurp@gmail.com>
2022-04-17 00:11:24 -0500

    still struggling with python, when will it end?

--- data-python/data_presenter.py
+++ data-python/data_presenter.py
@@ -12,3 +12,28 @@ for row in data:
     values = row.split(',')
     total = int(values[3]) * float(values[4])
     print(total)
+
+total = 0
+
+for row in data:
+    values = row.split(',')
+    total = total + (int(values[3]) * float(values[4]))
+
+print(total)
+
+
+data.close()
+
+x = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+
+y = [10,40,32,84,60,52,18]
+
+data.plot(x, y)
+
+data.xlabel('Day Purchased')
+
+data.ylabel('Cupcakes Purchased')
+
+data.title('My Cupcake Sales')
+
+data.show()
