62f080ddd2f68cc3b48d6aa0f1bbbb902a64f6fe
Connor Prentice <connurp@gmail.com>
2022-05-20 21:55:50 -0500

    Unit-7-Assessment Final

--- Unit-7-Assessment/new.js
+++ Unit-7-Assessment/new.js
@@ -26,5 +26,60 @@ function hasUniqueChars(word) {
   return response;
 }
 
-// console.log(addsToZero([1, 2, 3, -21, -3]));
+function isPangram(sentence) {
+  let alphabetArr = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let sentenceArr = sentence.replaceAll(" ", "").split("");
+  sentenceArr.forEach((letter) => {
+    remove(alphabetArr, letter);
+  });
+  return alphabetArr.length === 0;
+}
+
+function remove(arr, what) {
+  var found = arr.indexOf(what);
+
+  while (found !== -1) {
+    arr.splice(found, 1);
+    found = arr.indexOf(what);
+  }
+}
+
+function find_longest_word(wordList) {
+  var wordLength = wordList.map(function (wordList) {
+    return wordList.length;
+  });
+  wordLength = wordLength.sort((a, b) => b - a);
+  return wordLength[0];
+}
+
+console.log(find_longest_word(["hi", "because", "hello"]));
+console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
+console.log(addsToZero([1, 2, 3, -21, -3]));
 console.log(hasUniqueChars("baboon"));
