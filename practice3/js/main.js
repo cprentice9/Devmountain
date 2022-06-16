64b8f6e20d1890341ce73be45219f7cbee5c6376
Connor Prentice <connurp@gmail.com>
2022-06-16 16:22:09 -0400

    practice WIP

--- practice3/js/main.js
+++ practice3/js/main.js
@@ -20,10 +20,25 @@ function showStuff(el) {
   return x;
 }
 
+function showMore(el) {
+  var elementID = document.getElementsByName(el);
+  // console.log(elementID.length);
+  var myCheck = "";
+  for (i = 0; i < elementID.length; i++) {
+    // console.log(elementID[i].checked);
+    // console.log(elementID[i].value);
+    if (elementID[i].checked === true) {
+      myCheck = myCheck + elementID[i].value + "<br>";
+    }
+  }
+
+  return myCheck;
+}
+
 function evaluatePage() {
   var feedbackState = showStuff("state");
   var feedbackHome = showStuff("home");
-  document.getElementById("output").innerHTML =
-    feedbackState + "<br>" + feedbackHome;
+  var feedbackLight = showMore("bulb");
+  document.getElementById("output").innerHTML = feedbackState + "<br>" + feedbackHome + "<br>" + feedbackLight;
   showStuff("home");
 }
