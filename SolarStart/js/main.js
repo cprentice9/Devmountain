b0122801cedfa0f1b07f8434e1d9b481f11b835a
Connor Prentice <connurp@gmail.com>
2022-06-27 18:05:11 -0500

    Solar Calculator Final

--- SolarStart/js/main.js
+++ SolarStart/js/main.js
@@ -47,10 +47,54 @@ function sunHours() {
   return hrs;
 }
 
+function calculatePanel() {
+  var userChoice = document.forms.solarForm.panel.selectedIndex;
+  var panelOptions = document.forms.solarForm.panel.options;
+  var power = panelOptions[userChoice].value;
+  var name = panelOptions[userChoice].text;
+  var x = [power, name];
+  return x;
+}
+
 function calculateSolar() {
   var dailyUseKw = addMonths("mpc");
-  console.log(dailyUseKw);
+  //   console.log(dailyUseKw);
 
   var sunHoursPerDay = sunHours();
-  console.log(sunHoursPerDay);
+  //   console.log(sunHoursPerDay);
+
+  var minKwNeeds = dailyUseKw / sunHoursPerDay;
+  //   console.log(minKwNeeds);
+
+  var realKwNeeds = minKwNeeds * 1.25;
+  //   console.log(realKwNeeds);
+
+  var realWattNeeds = realKwNeeds * 1000;
+  //   console.log(realWattNeeds);
+
+  var panelInfo = calculatePanel();
+  var panelOutput = panelInfo[0];
+  var panelName = panelInfo[1];
+  //   console.log(panelOutput);
+  //   console.log(panelName);
+
+  var panelsNeeded = Math.ceil(realWattNeeds / panelOutput);
+  //   console.log(panelsNeeded);
+
+  var feedback = "";
+  feedback +=
+    "<p>Based on your average daily usage of " +
+    Math.round(dailyUseKw) +
+    " kWh, you will need to purchase " +
+    panelsNeeded +
+    " " +
+    panelName +
+    " solar panels to offset 100% of your electricity bill.</p>";
+  feedback += "<h2>Additional Details</h2>";
+  feedback += "<p>Your average daily electricity consumption: " + Math.round(dailyUseKw) + " kWh per day.</p>";
+  feedback += "<p>Average sunshine hours per day: " + sunHoursPerDay + " hours.</p>";
+  feedback += "<p>Realistic watts needed per hour: " + Math.round(realWattNeeds) + " watts per hour.</p>";
+  feedback += "<p>The " + panelName + " panel you selected generates about " + panelOutput + " watts per hour.</p>";
+
+  document.getElementById("feedback").innerHTML = feedback;
 }
