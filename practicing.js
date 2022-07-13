ed62191f05ffcebb5b29ed5af5077e27afb632b8
Connor Prentice <connurp@gmail.com>
2022-07-12 21:47:23 -0500

    practice

---
+++ practicing.js
@@ -0,0 +1,16 @@
+// let number = 1;
+// while (number <= 250) {
+//   console.log(number);
+//   number++;
+// }
+
+let trainsOperational = 8;
+let trainNumber = 1;
+while (trainNumber <= trainsOperational) {
+  console.log("Train #" + trainNumber + " is running!");
+  trainNumber++;
+}
+
+for (let trainNumber = 1; trainNumber <= trainsOperational; trainNumber++) {
+  console.log("Train #" + trainNumber + " is running!");
+}
