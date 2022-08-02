941a7010957558e08a6197744c0acdb9da0018e1
Connor Prentice <connurp@gmail.com>
2022-08-02 17:19:05 -0500

    user directory lab

---
+++ user-directory/user-directory/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from './components/Main'
+import Header from './components/Header'
+import UserCard from './components/UserCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
