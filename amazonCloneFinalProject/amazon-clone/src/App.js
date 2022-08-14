252081e5774a2ca441c9e6e00e90e9cb599a1862
Connor Prentice <connurp@gmail.com>
2022-08-13 19:39:39 -0500

    Dev mountain final project wip

---
+++ amazonCloneFinalProject/amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header.js";
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      <h1>Hello World</h1>
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ amazonCloneFinalProject/amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRPcEy66RnF_U9VKLSwnui3XYhx_vvtII",
+  authDomain: "clone-8ace2.firebaseapp.com",
+  projectId: "clone-8ace2",
+  storageBucket: "clone-8ace2.appspot.com",
+  messagingSenderId: "818417644309",
+  appId: "1:818417644309:web:4a79f7b060f1aa66d16f1c",
+  measurementId: "G-KSCB2BMPX2",
+};
