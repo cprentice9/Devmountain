4abec9fb80563921425aeb93b292897ce0b3974b
Connor Prentice <connurp@gmail.com>
2022-08-15 21:03:40 -0500

    DM FINAL PROJECT AMAZON CLONE WIP

--- amazonCloneFinalProject/amazon-clone/src/App.js
+++ amazonCloneFinalProject/amazon-clone/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header.js";
+import Home from "./Home";
 
 function App() {
   return (
     // BEM
     <div className="app">
-      <h1>Hello World</h1>
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
