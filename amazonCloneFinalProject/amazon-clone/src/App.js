588ca3ba296c6676bac5f338d4a9f58a912f6cb8
Connor Prentice <connurp@gmail.com>
2022-08-16 20:15:55 -0500

    Started checkout page, linked amazon logo to return home, added styling to checkout page

--- amazonCloneFinalProject/amazon-clone/src/App.js
+++ amazonCloneFinalProject/amazon-clone/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={[<Header />, <Home />]} />
 
-          <Route path="/checkout" element={<Header />} />
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
         </Routes>
       </div>
     </Router>
