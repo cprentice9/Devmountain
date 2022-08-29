c632b37185d175e0bb3c415bc8ff1df97027bc6d
Connor Prentice <connurp@gmail.com>
2022-08-29 17:10:16 -0500

    added firebase authentication and sign in methods. linked the log in page to the header and created login pages.

--- amazonCloneFinalProject/amazon-clone/src/firebase.js
+++ amazonCloneFinalProject/amazon-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCRPcEy66RnF_U9VKLSwnui3XYhx_vvtII",
   authDomain: "clone-8ace2.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
   appId: "1:818417644309:web:4a79f7b060f1aa66d16f1c",
   measurementId: "G-KSCB2BMPX2",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
