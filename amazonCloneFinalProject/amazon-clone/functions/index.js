cdd3011ae511015ac8bca211b919db4fc6681650
Connor Prentice <connurp@gmail.com>
2022-09-19 01:55:09 -0500

    added stripe secret key

--- amazonCloneFinalProject/amazon-clone/functions/index.js
+++ amazonCloneFinalProject/amazon-clone/functions/index.js
@@ -1,3 +1,6 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const stripe = require("stripe")(
+  "sk_test_51Ljb6gGN8c40SfHzJo0cVo8msXoGG0lldOhw7giMVisWL0bsmbGOcsQ1hsmIbSLcvWqbsaWEmYxdF0VVOd9a2Jgc00Fd9rzSoB"
+);
