f8ed4bc37f46f92612c5c35185416f3a15422913
Connor Prentice <connurp@gmail.com>
2022-09-26 16:29:37 -0500

    added orders page and looped it in with the back end

--- amazonCloneFinalProject/amazon-clone/src/Payment.js
+++ amazonCloneFinalProject/amazon-clone/src/Payment.js
@@ -7,6 +7,7 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
 import axios from "./axios";
+import { db } from "./firebase";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -45,6 +46,12 @@ function Payment() {
         },
       })
       .then(({ paymentIntent }) => {
+        db.collection("users").doc(user?.uid).collection("orders").doc(paymentIntent.id).set({
+          basket: basket,
+          amount: paymentIntent.amount,
+          created: paymentIntent.created,
+        });
+
         setSucceeded(true);
         setError(null);
         setProcessing(false);
