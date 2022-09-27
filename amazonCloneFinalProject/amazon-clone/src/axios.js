b2e452c92ea7de79cb7cfb59671230b3bb1fe02f
Connor Prentice <connurp@gmail.com>
2022-09-26 20:42:33 -0500

    finished amazon clone

--- amazonCloneFinalProject/amazon-clone/src/axios.js
+++ amazonCloneFinalProject/amazon-clone/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-8ace2/us-central1/api", // API URL (cloud function)
+  baseURL: "https://us-central1-clone-8ace2.cloudfunctions.net/api", // API URL (cloud function)
 });
 
 export default instance;
+
+// http://localhost:5001/clone-8ace2/us-central1/api
